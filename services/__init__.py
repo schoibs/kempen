c359f916fa77ca89195996919c874acb5606207a
schoibs <choibingshen2001@gmail.com>
2026-05-15 19:10:49 +0800

    remove videopromptgenerator service

--- services/__init__.py
+++ services/__init__.py
@@ -1,11 +1,8 @@
 """Prompt generation services for campaign production."""
 
 from .storyboard_generator import StoryboardGeneratorService, StoryboardGeneratorServiceError
-from .video_prompt_generator import VideoPromptGeneratorServiceError, VideoPromptGeneratorService
 
 __all__ = [
     "StoryboardGeneratorService",
     "StoryboardGeneratorServiceError",
-    "VideoPromptGeneratorServiceError",
-    "VideoPromptGeneratorService",
 ]
